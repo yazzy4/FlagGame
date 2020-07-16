9ad99df87fa11c2d3f2a8aa30dc40fa8bb42e08d
yazzy4 <yazburrell@pursuit.org>
2020-07-16 13:47:33 -0400

    added images to buttons, assigned but images with function, added border width to images

--- FlagGame/ViewController.swift
+++ FlagGame/ViewController.swift
@@ -9,10 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score = 0
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        countries += ["estonia", "germany", "france", "ireland", "italy", "monoco", "nigeria", "poland", "russia", "spain", "uk", "us" ]
+        
+        button1.layer.borderWidth = 1
+        button2.layer.borderWidth = 1
+        button3.layer.borderWidth = 1
+        
+        askQuestion()
+    }
+    
+    func askQuestion(){
+        button1.setImage(UIImage(named: countries[0]), for: .normal)
+        button2.setImage(UIImage(named: countries[1]), for: .normal)
+        button3.setImage(UIImage(named: countries[2]), for: .normal)
     }
 
 
